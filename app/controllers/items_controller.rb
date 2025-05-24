50618df199f798579e96348c03d80450f202fa09
nn88hcs99 <kyoko_n113@yahoo.co.jp>
2025-05-24 19:16:09 +0900

    商品出品機能と手数料・利益の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,29 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      @item.image.attach(params[:item][:image]) 
+      redirect_to root_path
+    else
+      #Rails.logger.debug "Validation Errors: #{@item.errors.full_messages}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+
+    def item_params
+      params.require(:item).permit(:item_title, :item_description, :category_id, :item_condition_id, :shipping_fee_payer_id, :prefecture_id, :shipping_day_id, :sales_price).merge(user_id: current_user.id)
+    end
+    
 end
+
