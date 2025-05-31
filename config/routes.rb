4e8f2ea1df7912e99d3ef93971274a96ec65216d
nn88hcs99 <kyoko_n113@yahoo.co.jp>
2025-05-31 19:04:02 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-   resources :items, only: [:new, :create] do
-
+   #resources :items, only: [:new, :create, :show] do
+   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     #resources :orders,only: :create
     #collection do
       #get 'search'
     #end  
   
   #resources :users, only: :show
- end 
+  end 
  end
