89cd0ea370b8245dc16db1b9df71e7e8ac35c452
nn88hcs99 <kyoko_n113@yahoo.co.jp>
2025-06-01 19:43:06 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authorize_user!, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -10,11 +12,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.new(create_item_params)
     if @item.save
       redirect_to root_path
     else
@@ -22,10 +23,44 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
+  def destroy
+    item.destroy
+    redirect_to root_path
+  end
 
-  def item_params
+  def edit
+  end
+
+
+ def update
+  if current_user == @item.user
+    if @item.update(update_item_params) 
+      redirect_to @item
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  else
+    redirect_to root_path
+  end
+end
+
+private
+ def set_item
+    @item = Item.find(params[:id])
+  end
+
+ def create_item_params
     params.require(:item).permit(:image, :item_title, :item_description, :category_id, :item_condition_id,
-                                 :shipping_fee_payer_id, :prefecture_id, :shipping_day_id, :sales_price).merge(user_id: current_user.id)
+                                 :shipping_fee_payer_id, :prefecture_id, :shipping_day_id, :sales_price)
+                         .merge(user_id: current_user.id)
+ end
+ def update_item_params
+    params.require(:item).permit(:image, :item_title, :item_description, :category_id, :item_condition_id,
+                                 :shipping_fee_payer_id, :prefecture_id, :shipping_day_id, :sales_price)
+ end
+
+  def authorize_user!
+    #@item = Item.find(params[:id]) # 商品情報を取得
+    redirect_to root_path,  unless @item.user == current_user
   end
 end
