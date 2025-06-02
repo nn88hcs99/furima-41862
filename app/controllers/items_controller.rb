33e68a3440908b4919b62a5bf8d40a70699d2efc
nn88hcs99 <kyoko_n113@yahoo.co.jp>
2025-06-02 22:11:09 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,10 +23,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  #def destroy
-  #  @item.destroy
-  #  redirect_to root_path
-  #end
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
 
   def edit
   end
