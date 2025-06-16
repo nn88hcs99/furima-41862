f835f24f51b975fca572928e4043bcc1665a114d
nn88hcs99 <kyoko_n113@yahoo.co.jp>
2025-06-16 22:42:55 +0900

    不要な#コメントアウトの削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,11 +30,6 @@ class OrdersController < ApplicationController
     redirect_to root_path if @item.user == current_user || @item.order.present?
   end
 
-  # def order_params
-  # params.require(:order_address_form).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token)
-  #    .merge(user_id: current_user.id, item_id: @item.id, amount: @item.sales_price)
-  # end
-
   def order_params
     params.require(:order_address_form).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
           .merge(user_id: current_user.id, item_id: @item.id, amount: @item.sales_price, token: params[:token]) # ← token を params から直接追加
@@ -43,9 +38,9 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: order_params[:amount], # 商品の値段
-      card: order_params[:token], # カードトークン
-      currency: 'jpy' # 通貨の種類（日本円）
+      amount: order_params[:amount],
+      card: order_params[:token],
+      currency: 'jpy' 
     )
   end
 end
